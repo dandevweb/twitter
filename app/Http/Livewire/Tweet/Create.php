43cfaebe4a762dbe544306f55bf8fa9ad914dd1e
dandevweb <dandevweb@gmail.com>
2023-03-30 21:59:44 -0300

    should make sure that only authenticated users can tweet

--- app/Http/Livewire/Tweet/Create.php
+++ app/Http/Livewire/Tweet/Create.php
@@ -3,10 +3,13 @@
 namespace App\Http\Livewire\Tweet;
 
 use App\Models\Tweet;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Livewire\Component;
 
 class Create extends Component
 {
+    use AuthorizesRequests;
+
     public ?string $body = null;
 
     public function render()
@@ -16,6 +19,8 @@ class Create extends Component
 
     public function tweet(): void
     {
+        $this->authorize('create', Tweet::class);
+
         Tweet::query()->create([
             'body' => $this->body,
             'created_by' => auth()->id(),
